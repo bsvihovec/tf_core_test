fce67e433a050d4fe553257e505145e227da5f5a
bsvihovec <bsvihovec@yahoo.com>
2021-10-06 17:06:37 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,6 @@ data "template_file" "test" {
 }
 
 module "tf_module_test" {
- # source = "./sleepy-modules"
-  source = "git@github.ibm.com:gshamann/terraform_13_modules.git//sleepy-modules"
+  source = "https://dev-cm.globalcatalog.test.cloud.ibm.com/api/v1-beta/catalogs/54b7df75-df03-44dc-95c5-06822d514035/offerings/08a3aaab-54af-48e9-8285-b34451c7cc2b/source?archive=tgz&kind=terraform&version=0.0.1"
   module_sample_var = "************** ${var.sample_var} ********** "
 }
