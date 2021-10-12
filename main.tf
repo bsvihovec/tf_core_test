48410b80c8e8ce73fea75934d14db69cd7029548
bsvihovec <bsvihovec@yahoo.com>
2021-10-12 09:17:53 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,6 @@ data "template_file" "test" {
 }
 
 module "tf_module_test" {
-  source = "https://dev-cm.globalcatalog.test.cloud.ibm.com/api/v1-beta/catalogs/54b7df75-df03-44dc-95c5-06822d514035/offerings/08a3aaab-54af-48e9-8285-b34451c7cc2b/source?archive=tgz&kind=terraform&version=0.0.1"
+  source = "https://dev-cm.globalcatalog.test.cloud.ibm.com/api/v1-beta/catalogs/8154adb9-107c-4d78-83b7-eb92c6b73092/offerings/ca90653d-5a5b-4f7f-9a64-cb52c7b5a08c/source?archive=tgz&kind=terraform&version=0.0.1"
   module_sample_var = "************** ${var.sample_var} ********** "
 }
